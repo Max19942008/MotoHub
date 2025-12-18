11c8cdb94073c1e7b3706e6c0b6f8b906a5da659
Rakhimboev Bobur <boburrahimboyev2008@gmail.com>
2025-12-18 19:43:14 +0900

    feat: integration GraphQLModule and build first GraphQL query api

--- apps/moto-hub/src/app.module.ts
+++ apps/moto-hub/src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from "@nestjs/config";
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { DatabaseModule } from './database/database.mudule';
+import { AppResolver } from './app.resolver';
+
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(),
+      GraphQLModule.forRoot({
+      driver:ApolloDriver,
+      playground: true,
+      uploads: false,
+      autoSchemaFile:true,
+    }),
+    DatabaseModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,AppResolver],
 })
 export class AppModule {}
