ed4b9b3f24bc2a0d0c479783f26fcad50b1ac93d
Rakhimboev Bobur <boburrahimboyev2008@gmail.com>
2025-12-20 11:39:48 +0900

    feat: develop logging via global interceptors

---
+++ apps/moto-hub/src/libs/interceptor/Logging.interceptor.ts
@@ -0,0 +1,54 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Logger,
+} from "@nestjs/common";
+import { GqlContextType, GqlExecutionContext } from "@nestjs/graphql";
+import { stringify } from "querystring";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger: Logger = new Logger();
+
+  public intercept(
+    context: ExecutionContext,
+    next: CallHandler
+  ): Observable<any> {
+    const recordTime = Date.now();
+    const requestType = context.getType<GqlContextType>();
+
+    if (requestType === "http") {
+      // Develop if needed
+      return next.handle().pipe();
+    } else if (requestType === "graphql") {
+      /** (1) Print Request **/
+      const gqlContext = GqlExecutionContext.create(context);
+      this.logger.log(
+        `${this.stringify(gqlContext.getContext().req.body)}`,
+        "REQUEST"
+      );
+
+      /** (2) Error handling vis GraphQL **/
+
+      /** (3) No Errors giving Response  **/
+      return next.handle().pipe(
+        tap((context) => {
+          const responseTime = Date.now() - recordTime;
+          this.logger.log(
+            `${this.stringify(context)}  ${responseTime}ms \n\n`,
+            "RESPONSE"
+          );
+        })
+      );
+    }
+  }
+  private stringify(context: ExecutionContext): string {
+    console.log(typeof context);
+
+    return JSON.stringify(context).slice(0, 75);
+  }
+}
