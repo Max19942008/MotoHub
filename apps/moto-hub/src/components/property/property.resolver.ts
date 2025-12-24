a9baa83f255c32b3a017e607e6faf3c6796304fd
Rakhimboev Bobur <boburrahimboyev2008@gmail.com>
2025-12-24 17:29:36 +0900

    feat: develop removePropertyByAdmin

--- apps/moto-hub/src/components/property/property.resolver.ts
+++ apps/moto-hub/src/components/property/property.resolver.ts
@@ -133,14 +133,14 @@ public async updatePropertyByAdmin(@Args("input") input: PropertyUpdate
   return await this.propertyService.updatePropertyByAdmin(input);
 };
 
-// @Roles(MemberType.ADMIN)
-// @UseGuards(RolesGuard)
-// @Mutation((returns) => Property)
-// public async removePropertyByAdmin(@Args('propertyId') input: string): Promise<Property> {
-//   console.log('Mutation: removePropertyByAdmin');
-//   const propertyId = shapeIntoMongoObjectId(input);
-//   return await this.propertyService.removePropertyByAdmin(propertyId);
+@Roles(MemberType.ADMIN)
+@UseGuards(RolesGuard)
+@Mutation((returns) => Property)
+public async removePropertyByAdmin(@Args('propertyId') input: string): Promise<Property> {
+  console.log('Mutation: removePropertyByAdmin');
+  const propertyId = shapeIntoMongoObjectId(input);
+  return await this.propertyService.removePropertyByAdmin(propertyId);
 
-// }
+};
 
 }
